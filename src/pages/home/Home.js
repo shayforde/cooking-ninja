5d730feb828eb796394354eb373d26268bf60df1
shay <shayforde@gmail.com>
2023-02-15 11:09:41 +0000

    number two

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,24 @@
+import { useFetch } from "../../hooks/useFetch"
+
 // styles
 import "./Home.css"
 
 export default function Home() {
-  return <div>Home</div>
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes")
+
+  console.log(data)
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Is Loading...</p>}
+      {data &&
+        data.map((recipe) => (
+          <div key={recipe.id}>
+            <h2>{recipe.title}</h2>
+            {/* <p>{recipe.method}</p> */}
+          </div>
+        ))}
+    </div>
+  )
 }
