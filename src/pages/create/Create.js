914a14ff0b6030efd367b1584c92609a22bed1a0
Shay Forde <shayforde@gmail.com>
2023-02-19 22:12:35 +0000

    about to do lesson 90

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,109 @@
+import { useState, useRef, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useFetch } from "../../hooks/useFetch"
+
 // styles
 import "./Create.css"
 
 export default function Create() {
-  return <div>Create</div>
+  const [title, setTitle] = useState("")
+  const [method, setMethod] = useState("")
+  const [cookingTime, setCookingTime] = useState("")
+  const [newIngredient, setNewIngredient] = useState("")
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
+
+  const navigate = useNavigate()
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  )
+
+  useEffect(() => {
+    if (data) {
+      navigate("/")
+    }
+  }, [data, navigate])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + " minutes",
+    })
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing])
+    }
+    setNewIngredient("")
+    ingredientInput.current.focus()
+  }
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        {error && <p className="error">{error}</p>}
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{" "}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">submit</button>
+      </form>
+    </div>
+  )
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,12 +1,12 @@
-import { useParams } from "react-router-dom"
-import { useFetch } from "../../hooks/useFetch"
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 
 // styles
-import "./Recipe.css"
+import './Recipe.css'
 
 export default function Recipe() {
   const { id } = useParams()
-  const url = "http://localhost:3000/recipes/" + id
+  const url = 'http://localhost:3000/recipes/' + id
   const { error, isPending, data: recipe } = useFetch(url)
 
   return (
